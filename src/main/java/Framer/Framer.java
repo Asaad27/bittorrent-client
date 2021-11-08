5d955f6861763b87de61a168c9d0446abd54f9d6
Youssef Boulkhir <youssef.boulkhir@grenoble-inp.org>
2021-11-08 21:05:17 +0000

    Delete Framer.java

--- src/main/java/Framer/Framer.java
+++
@@ -1,7 +0,0 @@
-package Framer;
-import java.io.IOException;
-import java.io.OutputStream;
-public interface Framer {
-	void frameMessage(byte[] msg, OutputStream out) throws IOException;
-	byte[] ReadnextMsg() throws IOException;
-}
